99bd39604dc72304cadb13543d367af364c470a0
Ivan Chen <lawliet357@gmail.com>
2021-10-01 13:24:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'psutil',
         'scipy',
         'seaborn==0.8.1',
-        'torch==1.3.1',
         'tqdm'
     ],
     description="Teaching tools for introducing people to deep RL.",
